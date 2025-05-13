f5cad538e1147c9b874c91ab758abb54a6e6a24a
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-13 13:00:36 +0700

    Add files via upload

---
+++ client/src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { CartProvider } from "@/context/CartContext";
+import { WishlistProvider } from "@/context/WishlistContext";
+import { AuthProvider } from "@/context/AuthContext";
+
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <CartProvider>
+      <WishlistProvider>
+        <App />
+      </WishlistProvider>
+    </CartProvider>
+  </AuthProvider>
+);
