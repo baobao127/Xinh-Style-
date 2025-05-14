5bb2cd53180546b69ba57ee3c4ffa466d020cd93
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-14 18:52:11 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,15 @@ import { CartProvider } from "@/context/CartContext";
 import { WishlistProvider } from "@/context/WishlistContext";
 import { AuthProvider } from "@/context/AuthContext";
 
-createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <CartProvider>
-      <WishlistProvider>
-        <App />
-      </WishlistProvider>
-    </CartProvider>
-  </AuthProvider>
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
