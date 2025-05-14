504289dcc122b0ae6ea5db5957d4cbad64c4e419
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-14 19:05:26 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,3 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { CartProvider } from "@/context/CartContext";
-import { WishlistProvider } from "@/context/WishlistContext";
-import { AuthProvider } from "@/context/AuthContext";
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
