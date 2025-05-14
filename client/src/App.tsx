7e38cdbf3febdaa38049056dfde73ad5f370769d
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-14 18:00:55 +0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import Home from "@/pages/Home";
-import Products from "@/pages/Products";
-import Cart from "@/pages/Cart";
-import NotFound from "@/pages/NotFound";
-import Layout from "@/components/layout/Layout";
-import AppRoutes from "@/AppRoutes";
 import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "@/AppRoutes";
 
 function App() {
   return (
@@ -17,15 +12,3 @@ function App() {
 
 export default App;
 
-export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/products" component={Products} />
-        <Route path="/cart" component={Cart} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  );
-}
