a015446a3878ed4d8e672b3fb80034cb5db47b8d
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-15 12:50:26 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,19 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
+
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: "class",
   content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
