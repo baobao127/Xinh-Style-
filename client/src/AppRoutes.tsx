963585f620ac692cc37245ccf0db0c07846f871c
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-15 20:38:41 +0700

    Update AppRoutes.tsx

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -2,37 +2,35 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from '@/components/layout/Layout';
-import Home from '@/pages/Home';
-import Products from '@/pages/Products';
-<Route path="/" element={<Layout />}>
-  <Route path="products" element={<Products />} />
-</Route>
-import ProductsView from '@/pages/ProductsView';
-<Route path="/" element={<Layout />}>
-  <Route path="products" element={<ProductsView />} />
-</Route>
-import Cart from '@/pages/Cart';
 import NotFound from '@/pages/NotFound';
 import Checkout from '@/pages/Checkout';
 import Success from '@/pages/Success';
-import AdminProducts from '@/pages/admin/AdminProducts';
+import Cart from '@/pages/Cart';
+import Layout from '@/components/layout/Layout';
+import Home from '@/pages/Home';
 
-<Route path="admin/products" element={<AdminProducts />} />
+// Khách
+import Products from '@/pages/Products';
+import ProductsView from '@/pages/ProductsView';
+
+// Admin
+import AdminProducts from '@/pages/admin/Products';
 import ProductsEditor from '@/pages/admin/ProductsEditor';
-
-<Route path="admin/products" element={<ProductsEditor />} />
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
+      {/* Route khách */}
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="products" element={<Products />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="success" element={<Success />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="all-products" element={<ProductsView />} />
+        {/* các route khác của khách */}
       </Route>
+
+      {/* Route admin */}
+      <Route path="/admin/products" element={<AdminProducts />} />
+      <Route path="/admin/product-editor" element={<ProductsEditor />} />
     </Routes>
   );
 };
