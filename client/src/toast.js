f11ef31925a5fee0c2629d7fd8acfc64cff223cb
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-16 11:05:10 +0700

    Delete client/src/toast.js

--- client/src/toast.js
+++
@@ -1,9 +0,0 @@
-
-// toast.js
-export function showToast(message) {
-  const toast = document.createElement("div");
-  toast.className = "custom-toast";
-  toast.textContent = message;
-  document.body.appendChild(toast);
-  setTimeout(() => toast.remove(), 3000);
-}
