0856313123f2fcb78c081852d1c726f49dc902ce
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-16 22:11:38 +0700

    Update AppRoutes.tsx

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -23,18 +23,13 @@ const AppRoutes: React.FC = () => {
         <Route path="success" element={<Success />} />
         <Route path="orders" element={<Orders />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/admin" element={<Dashboard />} />
-        <Route path="/admin/products" element={<ProductsEditor />} />
-        <Route path="/admin/orders" element={<AdminOrders />} />
-        <Route path="/admin/users" element={<AdminUsers />} />
       </Route>
+      <Route path="/admin" element={<Dashboard />} />
+      <Route path="/admin/products" element={<ProductsEditor />} />
+      <Route path="/admin/orders" element={<AdminOrders />} />
+      <Route path="/admin/users" element={<AdminUsers />} />
     </Routes>
   );
 };
 
 export default AppRoutes;
-
-
-
-
-
