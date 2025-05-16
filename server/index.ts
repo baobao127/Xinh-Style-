8413484a3a2e1fcfa759bb1aed8a7f44735785a1
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-16 10:39:35 +0700

    Delete server directory

--- server/index.ts
+++
@@ -1,9 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 phút
-  max: 100, // Tối đa 100 yêu cầu mỗi IP
-  message: "Bạn đã gửi quá nhiều yêu cầu. Vui lòng thử lại sau.",
-});
-
-app.use("/api", limiter); // Áp dụng middleware rate limiting cho tất cả các route API
