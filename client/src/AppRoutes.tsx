ff4d57f9de6e12c1ea1814fed7d6ddb187c328d6
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-16 21:34:00 +0700

    Create AppRoutes.tsx

---
+++ client/src/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from '@/components/layout/Layout';
+import Home from '@/pages/Home';
+import Products from '@/pages/Products';
+import ProductsView from '@/pages/ProductsView';
+import Cart from '@/pages/Cart';
+import Checkout from '@/pages/Checkout';
+import Success from '@/pages/Success';
+import Orders from '@/pages/Orders';
+import NotFound from '@/pages/NotFound';
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="all-products" element={<ProductsView />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="success" element={<Success />} />
+        <Route path="orders" element={<Orders />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
