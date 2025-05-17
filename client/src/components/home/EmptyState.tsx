6b6f07283ec8b2ecaa3b80144ef516b590a83a3e
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-17 20:43:15 +0700

    Create EmptyState.tsx

---
+++ client/src/components/home/EmptyState.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EmptyState: React.FC = () => {
+  return <p className="text-gray-500 text-center">Không có sản phẩm nào để hiển thị.</p>;
+};
+
+export default EmptyState;
